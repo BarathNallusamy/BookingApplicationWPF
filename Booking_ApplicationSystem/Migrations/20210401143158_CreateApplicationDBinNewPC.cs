32df41f81695150308857a54d3696dc9724e3175
BarathNallusamy <nallu.baray@gmail.com>
2021-04-01 15:40:40 +0100

    Add new classes from new pc

---
+++ Booking_ApplicationSystem/Migrations/20210401143158_CreateApplicationDBinNewPC.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Booking_ApplicationSystem.Migrations
+{
+    public partial class CreateApplicationDBinNewPC : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
